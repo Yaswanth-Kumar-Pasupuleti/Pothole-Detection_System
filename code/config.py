94ede37872bc1d4f8b8578dc26c5fc3b281950fb
Yaswanth Kumar Pasupuleti <106443025+Yaswanth-Kumar-Pasupuleti@users.noreply.github.com>
2025-03-30 19:05:19 +0530

    Add files via upload

---
+++ code/config.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ROOT_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)'
+TEST_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)/testyash'
+PREDICTION_THRES = 0.8
+EPOCHS = 5
+MIN_SIZE = 800
+BATCH_SIZE = 2
+DEBUG = False # to visualize the images before training
